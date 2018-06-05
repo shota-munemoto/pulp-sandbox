bf6d83e37951888fe3d2af16a9901c0fa6ca3787
shota-munemoto <shota.munemoto@gmail.com>
2018-06-05 20:07:17 +0900

    read constraints from csv files

--- scheduling.py
+++ scheduling.py
@@ -37,107 +37,105 @@ P = [[r['勤務名'] for _, r in kinmus.sort_values(by='並び順').iterrows()]
      for _, kinmus in renzoku_kinshi_kinmus.groupby('並びID')]
 
 # 日dの勤務kにグループgから割り当てる職員数の下限。
+c1_df = pd.read_csv(
+    './data/c1.csv',
+    dtype={
+        '日付': str,
+        '勤務名': str,
+        'グループ名': str,
+        '割り当て職員数下限': int
+    })
 c1 = {
     d: {
-        'a': {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1,
-            'E': 1
-        },
-        'b': {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1,
-            'E': 1
-        },
-        'c': {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1,
-            'E': 1
-        },
-        'd': {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1,
-            'E': 1
-        },
-        'e': {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1,
-            'E': 1
-        }
+        k: {r['グループ名']: r['割り当て職員数下限']
+            for _, r in groups.iterrows()}
+        for k, groups in kinmus.groupby('勤務名')
     }
-    for d in D
+    for d, kinmus in c1_df.groupby('日付')
 }
 # 日dの勤務kにグループgから割り当てる職員数の上限。
+c2_df = pd.read_csv(
+    './data/c2.csv',
+    dtype={
+        '日付': str,
+        '勤務名': str,
+        'グループ名': str,
+        '割り当て職員数上限': int
+    })
 c2 = {
     d: {
-        'a': {
-            'A': 2,
-            'B': 2,
-            'C': 2,
-            'D': 2,
-            'E': 2
-        },
-        'b': {
-            'A': 2,
-            'B': 2,
-            'C': 2,
-            'D': 2,
-            'E': 2
-        },
-        'c': {
-            'A': 2,
-            'B': 2,
-            'C': 2,
-            'D': 2,
-            'E': 2
-        },
-        'd': {
-            'A': 2,
-            'B': 2,
-            'C': 2,
-            'D': 2,
-            'E': 2
-        },
-        'e': {
-            'A': 2,
-            'B': 2,
-            'C': 2,
-            'D': 2,
-            'E': 2
-        }
+        k: {r['グループ名']: r['割り当て職員数上限']
+            for _, r in groups.iterrows()}
+        for k, groups in kinmus.groupby('勤務名')
     }
-    for d in D
+    for d, kinmus in c2_df.groupby('日付')
 }
 # 職員sの勤務kの割り当て数の下限。
-c3 = {s: {' ': 10} for s in S}
+c3_df = pd.read_csv(
+    './data/c3.csv', dtype={
+        '職員名': str,
+        '勤務名': str,
+        '割り当て数下限': int
+    })
+c3 = {
+    s: {r['勤務名']: r['割り当て数下限']
+        for _, r in kinmus.iterrows()}
+    for s, kinmus in c3_df.groupby('職員名')
+}
 # 職員sの勤務kの割り当て数の上限。
-c4 = {s: {} for s in S}
+c4_df = pd.read_csv(
+    './data/c4.csv', dtype={
+        '職員名': str,
+        '勤務名': str,
+        '割り当て数上限': int
+    })
+c4 = {
+    s: {r['勤務名']: r['割り当て数上限']
+        for _, r in kinmus.iterrows()}
+    for s, kinmus in c4_df.groupby('職員名')
+}
 # 勤務kの連続日数の下限。
-c5 = {}
+c5_df = pd.read_csv('./data/c5.csv', dtype={'勤務名': str, '連続日数下限': int})
+c5 = {r['勤務名']: r['連続日数下限'] for _, r in c5_df.iterrows()}
 # 勤務kの連続日数の上限。
-c6 = {'e': 2}
+c6_df = pd.read_csv('./data/c6.csv', dtype={'勤務名': str, '連続日数上限': int})
+c6 = {r['勤務名']: r['連続日数上限'] for _, r in c6_df.iterrows()}
 # 勤務kの間隔日数の下限。
-c7 = {' ': 2}
+c7_df = pd.read_csv('./data/c7.csv', dtype={'勤務名': str, '間隔日数下限': int})
+c7 = {r['勤務名']: r['間隔日数下限'] for _, r in c7_df.iterrows()}
 # 勤務kの間隔日数の上限。
-c8 = {' ': 3}
+c8_df = pd.read_csv('./data/c8.csv', dtype={'勤務名': str, '間隔日数上限': int})
+c8 = {r['勤務名']: r['間隔日数上限'] for _, r in c8_df.iterrows()}
 # 職員sの土日休暇回数の下限。
-c9 = {s: 1 for s in S}
+c9_df = pd.read_csv('./data/c9.csv', dtype={'職員名': str, '土日休暇回数下限': int})
+c9 = {r['職員名']: r['土日休暇回数下限'] for _, r in c9_df.iterrows()}
 # 職員sの土日休暇回数の上限。
-c10 = {}
-# 日dの職員sに割り当てる勤務k。
-c11 = {}
-# 日dの職員sに割り当てない勤務k。
-c12 = {}
+c10_df = pd.read_csv('./data/c10.csv', dtype={'職員名': str, '土日休暇回数上限': int})
+c10 = {r['職員名']: r['土日休暇回数上限'] for _, r in c10_df.iterrows()}
+# 職員sの日dに割り当てる勤務k。
+c11_df = pd.read_csv(
+    './data/c11.csv', dtype={
+        '職員名': str,
+        '日付': str,
+        '割り当て勤務名': str
+    })
+c11 = {
+    s: {r['日付']: r['割り当て勤務名']
+        for _, r in kinmus.iterrows()}
+    for s, kinmus in c11_df.groupby('職員名')
+}
+# 職員sの日dに割り当てない勤務k。
+c12_df = pd.read_csv(
+    './data/c12.csv', dtype={
+        '職員名': str,
+        '日付': str,
+        '割り当てない勤務名': str
+    })
+c12 = {
+    s: {r['日付']: r['割り当てない勤務名']
+        for _, r in kinmus.iterrows()}
+    for s, kinmus in c12_df.groupby('職員名')
+}
 
 # 決定変数。
 # 職員sの日dに勤務kが割り当てられているとき1。
