7c2456c5414a749a2f7ef7b14700b5ddae3e6490
shota-munemoto <shota.munemoto@gmail.com>
2018-06-11 21:23:42 +0900

    run create-react-app development server

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import multiprocessing
+import os
+import signal
+import subprocess
 import webbrowser
+import psutil
 import tkinter
 import tkinter.font
 import server
@@ -14,6 +18,12 @@ def run_server():
     server.run(host, port)
 
 
+def run_development_static():
+    project_root_directory = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(os.path.join(project_root_directory, 'static'))
+    subprocess.run(['npm', 'start'])
+
+
 def browser_opener(url):
     def open_browser():
         webbrowser.open_new_tab(url)
@@ -21,13 +31,26 @@ def browser_opener(url):
     return open_browser
 
 
-def terminator(tkinter_root, server_process):
-    def terminate_all():
-        server_process.terminate()
-        server_process.join()
-        tkinter_root.destroy()
-
-    return terminate_all
+# http://psutil.readthedocs.io/en/latest/#kill-process-tree
+def kill_proc_tree(pid,
+                   sig=signal.SIGTERM,
+                   include_parent=False,
+                   timeout=None,
+                   on_terminate=None):
+    """Kill a process tree (including grandchildren) with signal
+    "sig" and return a (gone, still_alive) tuple.
+    "on_terminate", if specified, is a callabck function which is
+    called as soon as a child terminates.
+    """
+    parent = psutil.Process(pid)
+    children = parent.children(recursive=True)
+    if include_parent:
+        children.append(parent)
+    for p in children:
+        p.send_signal(sig)
+    gone, alive = psutil.wait_procs(
+        children, timeout=timeout, callback=on_terminate)
+    return (gone, alive)
 
 
 if __name__ == '__main__':
@@ -44,10 +67,18 @@ if __name__ == '__main__':
 
     if utils.frozen():
         open_browser()
+    else:
+        development_static_process = multiprocessing.Process(
+            target=run_development_static)
+        development_static_process.start()
 
     root = tkinter.Tk()
     root.title('scheduling')
-    terminate_all = terminator(root, server_process)
+
+    def terminate_all():
+        kill_proc_tree(os.getpid())
+        root.destroy()
+
     root.protocol('WM_DELETE_WINDOW', terminate_all)
     frame = tkinter.Frame(root)
     frame.pack()
@@ -58,4 +89,11 @@ if __name__ == '__main__':
     terminate_button = tkinter.Button(
         frame, command=terminate_all, text='終了する。', font=font)
     terminate_button.pack(fill='x')
+
+    def handler(signal_number, stack_frame):
+        terminate_all()
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+
     root.mainloop()
